3368967b57fe83cca8f531ee31b3af207116e850
Haleian81 <jodeauxstudios@gmail.com>
2025-01-21 21:14:14 -0500

    bug fix

--- pages/index.js
+++ pages/index.js
@@ -62,12 +62,12 @@ export default function Home() {
       {/* Hero Section */}
       <header id="hero" className={`${styles.section} ${styles.hero}`}>
 
-<h1>Crafting Immersive Digital Worlds</h1>
+      <h1>Crafting Immersive Digital Worlds</h1>
 <h2>Solutions that Engage and Delight</h2>
-<h3>I'm a full-stack developer with a passion for building high-quality web applications. 
+<h3>I&apos;m a full-stack developer with a passion for building high-quality web applications. 
     My expertise spans the entire development lifecycle, from front-end experiences with React, 
     Vue, and Angular to robust backends powered by Node.js, Python (Django, Flask), and PHP (Laravel).
-    </h3>
+</h3>
       </header>
 
       {/* Three.js Canvas */}
